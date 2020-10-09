d374b20b9edf67d6132f0eac24e07b4dd21568d7
Артём Мещерин <72318993+st091150@users.noreply.github.com>
2020-10-09 18:35:29 +0430

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+int main()
+{
+    int n = 10;
+    std::fstream outFile;
+    outFile.open("File.txt");
+    if (!outFile) {
+        std::cout << "Can not open File1" << std::endl;
+    }
+    char b[ sizeof(n)*2 ];
+        std::cout << ">";
+        std::cin.getline(b,sizeof(b));
+        outFile << b;
+    outFile.close();
+    outFile.open("File.txt");
+    if (!outFile) {
+        std::cout << "Can not open File1" << std::endl;
+    }
+    std::fstream inFile;
+    inFile.open("File2.txt");
+    if (!inFile) {
+        std::cout << "Can not open File2" << std::endl;
+    }
+    char d[sizeof(n) * 2];
+    while (outFile.peek() != EOF) {
+        outFile >> d;
+        inFile << d;
+    }
+    outFile.close();
+    inFile.close();
+}
+ 
